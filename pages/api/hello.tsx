e7c7dbcf075db8a738b110be5a1326f64403afff
Kevin O'Rourke <orourke.kevin@gmail.com>
2021-01-04 07:40:11 -0800

    Convert .js files to .tsx

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,6 +1,10 @@
+import { NextApiHandler } from 'next';
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+const handler: NextApiHandler = (req, res) => {
   res.statusCode = 200;
   res.json({ name: 'John Doe' });
 };
+
+export default handler;
