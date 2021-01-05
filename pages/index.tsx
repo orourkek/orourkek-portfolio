24c542cba015b13f08403882e10c6dc903800644
Kevin O'Rourke <orourke.kevin@gmail.com>
2021-01-05 09:27:09 -0800

    Add bridge pic + basic home layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+
+const GFONT_FAMILY = 'Montserrat:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600';
 
 export default function Home() {
   return (
@@ -7,12 +10,23 @@ export default function Home() {
       <Head>
         <title>Kevin O'Rourke</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href={`https://fonts.googleapis.com/css2?family=${GFONT_FAMILY}`}
+        />
       </Head>
 
       <main className={styles.main}>
         <section className={styles.hero}>
-          <h1>Hello World</h1>
+          <Image src="/bridge.jpg" layout="fill" objectFit="cover" />
+          <div className={styles.bgOverlay} />
+          <header>
+            <h1>Kevin O'Rourke</h1>
+          </header>
         </section>
+
+        <section className={styles.foo}>Hello</section>
       </main>
     </>
   );
